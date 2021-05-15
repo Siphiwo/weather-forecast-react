90a075485ab5139c38e908f654ca7c46f60e9cf3
siphiwo <siphiwojulayi462@gmail>
2021-05-15 19:53:30 +0200

    menu ping animation added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 function Header() {
     return (
         <ul className="flex ml-auto w-full font-bold">
-            <li className="text-xs text-gray-800 ml-auto mr-6">Weather</li>
-            <li className="text-xs text-gray-800 mr-6">Alerts</li>
+            <li className="text-xs text-gray-800 ml-auto mr-6 border-b-2 border-green-400">Weather</li>
+            <li className="text-xs text-gray-800 mr-6 alert-notice">Alerts</li>
             <li className="text-xs text-gray-800 mr-6">Map</li>
             <li className="text-xs text-gray-800 mr-6">Satellite</li>
             <li className="text-xs text-gray-800">News</li>
